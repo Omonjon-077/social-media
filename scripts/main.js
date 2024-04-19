5d6ed520a153b21b23f98707bd0a05dd2e20c676
Omonjon <omonjons077@gmail.com>
2024-04-19 20:15:22 +0500

    Site done to 768px responsive

---
+++ scripts/main.js
@@ -0,0 +1,53 @@
+/*================= ALL CONSTS  =================*/
+const menuItems = document.querySelectorAll('.menu-item');
+
+const messagesNotification = document.querySelector('#messages-notification'),
+      messages = document.querySelector('.messages'),
+      message = messages.querySelectorAll('.message'),
+      messageSearch = document.querySelector('#message-search');
+
+
+/*================= SIDEBAR  =================*/
+// Remove active class from all menu items
+const changeActiveItem = () => {
+    menuItems.forEach(item => {
+        item.classList.remove('active');
+    })
+}
+menuItems.forEach(item => {
+    item.addEventListener('click', () => {
+        changeActiveItem();
+        item.classList.add('active');
+        if (item.id != 'notifications') {
+            document.querySelector('.notifications-popup').style.display = 'none';
+        } else {
+            document.querySelector('.notifications-popup').style.display = 'block';
+            document.querySelector('#notifications .notification-count').style.display = 'none';
+        }
+    })
+})
+
+/*================= MESSAGES  =================*/
+// search chat
+const searchMessage = () => {
+    const val = messageSearch.value.toLowerCase();
+    message.forEach(user => {
+        let name = user.querySelector('h5').textContent.toLowerCase();
+        if(name.indexOf(val) != -1) {
+            user.style.display = 'flex';
+        } else {
+            user.style.display = 'none';
+        }
+    })
+}
+
+messageSearch.addEventListener('keyup', searchMessage)
+
+// hightlight messages card when messages menu item is clicked
+messagesNotification.addEventListener('click', () =>  {
+    messages.style.boxShadow = '0 0 1rem var(--color-primary)';
+    messagesNotification.querySelector('.notification-count').style.display = 'none';
+    setTimeout(() => {
+        messages.style.boxShadow = 'initial'
+    }, 2000)
+})
