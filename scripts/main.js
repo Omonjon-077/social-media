21115faa9b1bfc63bf177ecaac4f26cc61959938
Omonjon <omonjons077@gmail.com>
2024-04-20 15:26:16 +0500

    Site font size changer done

--- scripts/main.js
+++ scripts/main.js
@@ -2,18 +2,23 @@
 const menuItems = document.querySelectorAll('.menu-item');
 
 const messagesNotification = document.querySelector('#messages-notification'),
-      messages = document.querySelector('.messages'),
-      message = messages.querySelectorAll('.message'),
-      messageSearch = document.querySelector('#message-search');
+    messages = document.querySelector('.messages'),
+    message = messages.querySelectorAll('.message'),
+    messageSearch = document.querySelector('#message-search');
 
+const theme = document.querySelector('#theme'),
+    themeModal = document.querySelector('.customize-theme');
 
+const fontSizes = document.querySelectorAll('.choose-size span');
+ let root = document.querySelector(':root');
 /*================= SIDEBAR  =================*/
 // Remove active class from all menu items
-const changeActiveItem = () => {
-    menuItems.forEach(item => {
-        item.classList.remove('active');
-    })
-}
+const
+    changeActiveItem = () => {
+        menuItems.forEach(item => {
+            item.classList.remove('active');
+        })
+    }
 menuItems.forEach(item => {
     item.addEventListener('click', () => {
         changeActiveItem();
@@ -33,7 +38,7 @@ const searchMessage = () => {
     const val = messageSearch.value.toLowerCase();
     message.forEach(user => {
         let name = user.querySelector('h5').textContent.toLowerCase();
-        if(name.indexOf(val) != -1) {
+        if (name.indexOf(val) != -1) {
             user.style.display = 'flex';
         } else {
             user.style.display = 'none';
@@ -44,10 +49,65 @@ const searchMessage = () => {
 messageSearch.addEventListener('keyup', searchMessage)
 
 // hightlight messages card when messages menu item is clicked
-messagesNotification.addEventListener('click', () =>  {
+messagesNotification.addEventListener('click', () => {
     messages.style.boxShadow = '0 0 1rem var(--color-primary)';
     messagesNotification.querySelector('.notification-count').style.display = 'none';
     setTimeout(() => {
         messages.style.boxShadow = 'initial'
     }, 2000)
+})
+
+/*================= THEME/DISPLAY CUSTOMIZATION  =================*/
+// opens modal
+const openThemeModal = () => {
+    themeModal.style.display = 'grid';
+}
+
+// close modal
+const closeThemeModal = (e) => {
+    if (e.target.classList.contains('customize-theme')) {
+        themeModal.style.display = 'none';
+    }
+}
+
+themeModal.addEventListener('click', closeThemeModal);
+theme.addEventListener('click', openThemeModal);
+
+/*================= FONT SIZE  =================*/
+// remove active class from spans or font size selectors
+const removeSizeSelector = () => {
+   fontSizes.forEach(size => {
+       size.classList.remove('active');
+   })
+}
+fontSizes.forEach(size => {
+    size.addEventListener('click', () => {
+        let fontSize;
+        removeSizeSelector();
+        size.classList.toggle('active');
+        if (size.classList.contains('font-size-1')) {
+            fontSize = '10px';
+            root.style.setProperty('--sticky-top-left', '5.4rem');
+            root.style.setProperty('--sticky-top-right', '5.4rem');
+        } else if (size.classList.contains('font-size-2')) {
+            fontSize = '13px';
+            root.style.setProperty('--sticky-top-left', '5.4rem');
+            root.style.setProperty('--sticky-top-right', '-7rem');
+        } else if (size.classList.contains('font-size-3')) {
+            fontSize = '16px';
+            root.style.setProperty('--sticky-top-left', '-2rem');
+            root.style.setProperty('--sticky-top-right', '-17rem');
+        } else if (size.classList.contains('font-size-4')) {
+            fontSize = '19px';
+            root.style.setProperty('--sticky-top-left', '-5rem');
+            root.style.setProperty('--sticky-top-right', '-25rrem');
+        } else if (size.classList.contains('font-size-5')) {
+            fontSize = '22px';
+            root.style.setProperty('--sticky-top-left', '-12rem');
+            root.style.setProperty('--sticky-top-right', '-35rem');
+        }
+
+        // change font size of the toor html element
+        document.querySelector('html').style.fontSize = fontSize;
+    })
 })
